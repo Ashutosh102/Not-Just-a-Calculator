9afa102634286535ae73870608c151b9a1a91805
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2024-09-22 03:39:08 +0530

    Update index.tsx

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -274,7 +274,7 @@ export default function Home() {
                 <Draggable
                     key={index}
                     defaultPosition={latexPosition}
-                    onStop={(e, data) => setLatexPosition({ x: data.x, y: data.y })}
+                    onStop={(_, data) => setLatexPosition({ x: data.x, y: data.y })}
                 >
                     <div className="absolute p-2 text-white rounded shadow-md">
                         <div className="latex-content">{latex}</div>
